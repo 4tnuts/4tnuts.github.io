1419b95b773c8ba1b69525f376787dc636df0538
4tnuts <ramanavami32@gmail.com>
2020-06-20 19:51:55 +0800

    added theme changer according to local time

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,29 @@
 let darkButton = document.getElementById("dark");
 let lightButton = document.getElementById("light");
 let body = document.getElementsByTagName("body");
-console.log(body[0].classList)
+let dateTime = new Date();
+let timeHour = dateTime.getHours();
+
+const toDark = () => {
+  body[0].classList.add("dark");
+  body[0].classList.remove("light");
+};
+
+const toLight = () => {
+  body[0].classList.add("light");
+  body[0].classList.remove("dark");
+};
+
+if (timeHour >= 18) {
+  toDark();
+} else {
+  toLight();
+}
 
 darkButton.onclick = () => {
-    body[0].classList.add("dark");
-    body[0].classList.remove("light");
-    console.log(body[0].classList)
-}
+  toDark();
+};
 
 lightButton.onclick = () => {
-    body[0].classList.add("light");
-    body[0].classList.remove("dark");
-    console.log(body[0].classList)
-}
-
+  toLight();
+};
