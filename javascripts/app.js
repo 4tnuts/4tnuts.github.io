388240d3932ccc2529c13a9fe5ac3acc88afd614
4tnuts <ramanavami32@gmail.com>
2020-06-28 19:20:21 +0800

    change my nickname

--- javascripts/app.js
+++ javascripts/app.js
@@ -36,20 +36,15 @@ window.onscroll = function fadeIn() {
         let bottomPart = sectionSize.bottom;
         let topPart = sectionSize.top;
         console.log(`ini bottom part ${bottomPart} & ini window scroll y ${window.scrollY} & ini top part ${topPart}`)
-        console.log(typeof bottomPart);
-        console.log(bottomPart < 50);
-        if ( bottomPart < 50) {
-          section.style.opacity = "0";
-          section.style.transform = "translateX(-20px)";
-          section.style.transition = "1s ease-in-out";
-            
-        }else if(window.scrollY >= bottomPart){
-          console.log("masuk 2")
+        if (window.scrollY >= bottomPart) {
             section.style.opacity = "1";
             section.style.transform = "translateX(0)";
             section.style.transition = "1s ease-in-out";
+        } else if(bottomPart < 0){
+            section.style.opacity = "0";
+            section.style.transform = "translateX(-20px)";
+            section.style.transition = "1s ease-in-out";
         }else{
-          console.log("masuk 3")
           section.style.opacity = "0";
             section.style.transform = "translateX(-20px)";
             section.style.transition = "1s ease-in-out";
